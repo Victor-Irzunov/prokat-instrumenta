ea30080eb056412e03c39b9e08acc34cda811a7a
Виктор <viktor@MacBook-Air-Viktor.local>
2024-02-20 18:49:19 +0300

    --4

--- components/Otzyvy/Otzyvy.js
+++ components/Otzyvy/Otzyvy.js
@@ -64,11 +64,11 @@ const Otzyvy = () => {
 									{otzyv.name}
 								</p>
 								<div className="rating mb-3">
-									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" />
-									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" />
-									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" />
-									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" />
-									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" />
+									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" onChange={()=>{}} />
+									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" onChange={()=>{}} />
+									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" onChange={()=>{}}/>
+									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" onChange={()=>{}} />
+									<input type="radio" name="rating-2" className="mask mask-star-2 bg-primary" onChange={()=>{}} />
 								</div>
 								<p className='text-sm text-gray-600'>
 									{otzyv.text}
